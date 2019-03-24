c1742822c27d30496d907cd35d37d2b2f62e4838
pulok <mdmunifhasan8520@gmail.com>
2019-03-24 21:55:21 +0600

    collection view cell hide

--- AutoLayout/Controller/ViewController.swift
+++ AutoLayout/Controller/ViewController.swift
@@ -43,6 +43,7 @@ class ViewController: UIViewController {
     
     @IBAction func userPressedAction(_ sender: UIButton) {
         print("hello\(sender.tag)")
+        hintsCollectionView.isHidden = true
     }
        
     
@@ -64,9 +65,10 @@ class ViewController: UIViewController {
             answerCollectionView?.delegate = self
             answerCollectionView?.dataSource = self
         
-        hintsCollectionView?.delegate = self
-        hintsCollectionView?.dataSource = self
+            hintsCollectionView?.delegate = self
+            hintsCollectionView?.dataSource = self
         
+      
         
             answerString = firstQuestion.answerText
 
@@ -139,6 +141,7 @@ class ViewController: UIViewController {
 }
 
 //answer collectionView
+
 // MARK: CollectionView DataSource
 extension ViewController: UICollectionViewDataSource {
     
@@ -155,19 +158,23 @@ extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
       
             if collectionView == self.answerCollectionView  {
-                let cell1 = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! answerCell
-                cell1.answerButton.setTitle(answerDict[indexPath.row], for: .normal)
-                cell1.answerButton.setBackgroundImage(UIImage(named: "letter"), for: .normal)
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! answerCell
+                cell.answerButton.setTitle(answerDict[indexPath.row], for: .normal)
+                cell.answerButton.setBackgroundImage(UIImage(named: "letter"), for: .normal)
                 
-                return cell1
+                return cell
                 
             } else {
-                let cell2 = collectionView.dequeueReusableCell(withReuseIdentifier: "HINTS", for: indexPath) as! hintsCell
-                
-                cell2.hintsButton.setTitle(randomDict[indexPath.row], for: .normal)
-                cell2.hintsButton.setBackgroundImage(UIImage(named: "letter"), for: .normal)
-                return cell2
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HINTS", for: indexPath) as! hintsCell
+               
+                cell.hintsButton.setTitle(randomDict[indexPath.row], for: .normal)
+                cell.hintsButton.setBackgroundImage(UIImage(named: "letter"), for: .normal)
+                cell.isSelected = true
+                hintsCollectionView.selectItem(at: indexPath, animated: true, scrollPosition: [])
+                return cell
+               
             }
+        
     }
 
 }
@@ -175,9 +182,36 @@ extension ViewController: UICollectionViewDataSource {
 // MARK: CollectionView Delegate
 extension ViewController: UICollectionViewDelegate {
     
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: NSIndexPath) {
-        print("User\(indexPath.item)")
+  func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HINTS", for: indexPath) as! hintsCell
+    
+            print("hello")
     }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+       
+        let cell = answerCollectionView.cellForItem(at: indexPath)
+        cell?.layer.borderWidth = 10
+        print("hello")
+        
+    }
+   
+    
+    
+    
+ 
+        
+        
+        
+        
+//        print("User\(indexPath.item)")
+//        let cell  = collectionView.dequeueReusableCell(withReuseIdentifier: "HINTS", for: indexPath as IndexPath) as! hintsCell
+//        //cell.hintsButton.isSelected = true
+//        cell.hintsButton.tag = 0
+//        let id = cell.hintsButton.tag
+//        print(id)
+     
+        
+    
 }
 
 
